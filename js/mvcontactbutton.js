9658a5404a5036fb090c422c7582f9f5bb725a6f
sfxon <steve@mindfav.com>
2022-10-25 20:30:16 +0200

    Improved the code quality.

--- js/mvcontactbutton.js
+++ js/mvcontactbutton.js
@@ -1,14 +1,14 @@
 jQuery( document ).ready(function($) {
-		$('.mvcontactbuttonform').hide();
-		
-		$('.mvcontactbutton').on('click', function(event) {
-				event.preventDefault();
-				event.stopPropagation();
-				
-				$('.mvcontactbuttonform').slideDown()
-				
-				$('html, body').animate({
-    				scrollTop: ($(".mvcontactbuttonform").offset().top)
-				},200);
-		});
+    $('.mvcontactbuttonform').hide();
+    
+    $('.mvcontactbutton').on('click', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        
+        $('.mvcontactbuttonform').slideDown()
+        
+        $('html, body').animate({
+            scrollTop: ($(".mvcontactbuttonform").offset().top)
+        },200);
+    });
 });
--- mvcontactbutton.php
+++ mvcontactbutton.php
@@ -34,48 +34,14 @@
 
  * Author email: info@mindfav.com
 
- * License: GPL 2
-
- * Donate URI: https://mindfav.com/donate/
-
- *
-
- *
+ * License: MIT
 
  * Copyright 2012-2020 Steve Krämer
 
- *
-
- * mvcontactbutton is free software: you can redistribute it and/or modify
-
- * it under the terms of the GNU General Public License, version 2, as published by
-
- * the Free Software Foundation.
-
- *
-
- * mvcontactbutton is distributed in the hope that it will be useful,
-
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
-
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-
- * GNU General Public License for more details.
-
- *
-
- * You should have received a copy of the GNU General Public License
-
- * along with WordPress. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
-
  */
- 
- 
-//Klassen:
-//mvcontactbutton				-> Der Button, der das Formular anzeigen soll
-//mvcontactbuttonform		-> Der Container rund um das Formular
-
-
+//Classes:
+//mvcontactbutton	-> Der Button, der das Formular anzeigen soll
+//mvcontactbuttonform	-> Der Container rund um das Formular
 
 // Prohibit direct script loading.
 defined( 'ABSPATH' ) || die( 'No direct script access allowed!' );
@@ -83,7 +49,7 @@ defined( 'ABSPATH' ) || die( 'No direct script access allowed!' );
 add_action( 'wp_enqueue_scripts', 'my_custom_script_load' );
 
 function my_custom_script_load(){
-  	wp_enqueue_script( 'my-custom-script', plugin_dir_url(__FILE__) . '/js/mvcontactbutton.js', array( 'jquery' ) );
+    wp_enqueue_script( 'my-custom-script', plugin_dir_url(__FILE__) . '/js/mvcontactbutton.js', array( 'jquery' ) );
 }
 
 
